f517bd32f6d6f2d19e234a84317317e2914cb9bc
timofey stepanov <125030768+FrozenQxc@users.noreply.github.com>
2023-04-01 00:10:01 +0300

    update

--- 2.py
+++ 2.py
@@ -3,10 +3,17 @@ length = len(string)
 count = 0
 
 for index, char in enumerate(string):
-    if char == "c":
-        count += 1
-        print("Найден символ 'c'")
+    if index == 2:
         continue
+    elif char == "c":
+        count += 1
+        print("Строка содержит символ 'c'")
+
+    if index < length-1:
+        print(char, end="")
+
+        if index == length - 3:
+            print("\nПропущен третий символ:", string[2], end="")
 
 print("\nКоличество символов 'c':", count)
 print("Длина строки:", length)

3640d653c66c9ab28347b7da686305fba5a4e8a3
timofey stepanov <125030768+FrozenQxc@users.noreply.github.com>
2023-04-07 17:54:40 +0300

    рефакторинг

--- 4.py
+++ 4.py
@@ -1,65 +1,68 @@
 import math
 import random
 
+print("Операции: +, -, /, *, ** (возведение в степень), mod (модуль), rand (рандомное число), fact (факториал), acos (арккосинус)")
+
 while True:
-    print("Операции: +, -, /, *, ** (возведение в степень), mod (модуль), rand (рандомное число), fact (факториал), acos (арккосинус)")
 
     choice = input("Введите операцию: ")
-
+    # Метод split используется для разбиения строки на список подстрок по определенному разделителю. В данном случае разделитель - это пробел.
+    # Метод map делает число вещественным
     if choice == "+":
-        num1 = float(input("Введите первое число: "))
-        num2 = float(input("Введите второе число: "))
+        num1, num2 = map(float, input("Введите два числа через пробел: ").split())
         print(num1 + num2)
         break
 
     elif choice == "-":
-        num1 = float(input("Введите первое число: "))
-        num2 = float(input("Введите второе число: "))
+        num1, num2 = map(float, input("Введите два числа через пробел: ").split())
         print(num1 - num2)
         break
 
-    elif choice == "/":
-        num1 = float(input("Введите первое число: "))
-        num2 = float(input("Введите второе число: "))
-        print(num1 / num2)
-        break
-
     elif choice == "*":
-        num1 = float(input("Введите первое число: "))
-        num2 = float(input("Введите второе число: "))
+        num1, num2 = map(float, input("Введите два числа через пробел: ").split())
         print(num1 * num2)
         break
 
+    elif choice == "/":
+        num1, num2 = map(float, input("Введите два числа через пробел: ").split())
+        if num2 != 0:
+            print(num1 / num2)
+            break
+        else:
+            print("Ошибка: деление на ноль.")
+
     elif choice == "**":
-        num1 = float(input("Введите число: "))
-        num2 = float(input("Введите степень: "))
+        num1, num2 = map(float, input("Введите число и степень через пробел: ").split())
         print(num1 ** num2)
         break
 
     elif choice == "mod":
         num = float(input("Введите число: "))
-
         result = abs(num)
         print("Результат:", result)
         break
 
     elif choice == "rand":
-        low = int(input("Введите нижнюю границу: "))
-        high = int(input("Введите верхнюю границу: "))
-
+        low, high = map(int, input("Введите нижнюю и верхнюю границы через пробел: ").split())
         result = random.uniform(low, high)
         print("Результат:", result)
         break
 
     elif choice == "fact":
         num = int(input("Введите число: "))
-        print(math.factorial(num))
-        break
+        if num >= 0:
+            print(math.factorial(num))
+            break
+        else:
+            print("Ошибка: факториал отрицательного числа не существует.")
 
     elif choice == "acos":
         num = float(input("Введите число: "))
-        print(math.acos(num))
-        break
+        if -1 <= num <= 1:
+            print(math.acos(num))
+            break
+        else:
+            print("Ошибка: введенное число должно быть в диапазоне от -1 до 1.")
 
     else:
         print ("Возникла ошибка.Try again")
--- практика/z1.py
+++ практика/z1.py
@@ -1,8 +1,16 @@
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 
+list = list(range(1,11))
+a.sort()
+print(a)
+a.sort(reverse=True)
+print(a)
+
+print(list[::2])
+
 
 print(list(filter(lambda elem: elem < 5, a)))
 
 # for elem in (a):
 #     if elem >5:
-#         print(elem)
+#         print(elem
